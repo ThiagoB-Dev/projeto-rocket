f8d0fe9d7b758cd97707c34fe2550d1ecc3549d3
Thiago Barbosa de Sobral Batista <thigas.tb360.tbs88@gmail.com>
2024-08-01 14:04:55 -0300

    add some links and image resizing

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ function toggleMode() {
   if(html.classList.contains('light')) {
     // se tiver light mode, adicionar a imagem light
      img.setAttribute('src', './assets/avatar-light.png')
-     img.setAttribute('alt', 'maykbrito de oculos escuro')
+     img.setAttribute('alt', 'thiago de oculos escuro')
   } else {
       // se tiver sem light mode, manter a imagem normal
       img.setAttribute('src', './assets/avatar.png')
-      img.setAttribute('alt', 'maykbrito com blusa preta')
+      img.setAttribute('alt', 'thiago com blusa preta')
   }
 }
